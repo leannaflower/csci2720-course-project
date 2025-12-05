a6ee29ce086ef4533e394f4d5ee4ff1b8ff69ba6
HTsz110 <132541006+HTsz110@users.noreply.github.com>
2025-12-06 02:15:57 +0800

    Task 4 User

--- csci2720-project-grp27/server/routes/venueRoutes.js
+++ csci2720-project-grp27/server/routes/venueRoutes.js
@@ -1,22 +1,11 @@
-import express from "express";
-import { getVenues } from "../controllers/venueController.js";
+const { Router } = require("express");
+const venueController = require("../controllers/venueController");
+const { authenticate } = require("../middleware/authenticate");
+const { authorize } = require("../middleware/authorize");
 
-// // start of a quick placeholder
-// import userRoutes from "./routes/userRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-// import eventRoutes from "./routes/eventRoutes.js";
-// import favouriteRoutes from "./routes/favouriteRoutes.js";
-// import commentRoutes from "./routes/commentRoutes.js";
+const router = Router();
 
-// app.use("/api/users", userRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/events", eventRoutes);
-// app.use("/api/favourites", favouriteRoutes);
-// app.use("/api/comments", commentRoutes);
-// // end of a quick placeholder
+router.get("/", authenticate, authorize("user", "admin"), venueController.listVenues);
+router.get("/:venueId", authenticate, authorize("user", "admin"), venueController.getVenueById);
 
-const router = express.Router();
-
-router.get("/", getVenues); // GET all venues
-
-export default router;
+module.exports = router;

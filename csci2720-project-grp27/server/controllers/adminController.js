7b056dc0517faa92ea13bb329bf97c9ea810c2a0
HTsz110 <132541006+HTsz110@users.noreply.github.com>
2025-12-06 02:11:41 +0800

    Task 4 auth

--- csci2720-project-grp27/server/controllers/adminController.js
+++ csci2720-project-grp27/server/controllers/adminController.js
@@ -1,5 +1,12 @@
-// quick placeholder
-// this file will contain the logic for what the API should do when admin's route is accessed
-export const placeholder = (req, res) => {
-    res.json({ message: "OK" });
-};
+import Venue from "../models/Venue.js";
+import Event from "../models/Event.js";
+
+export async function createVenue(req, res) {
+  const venue = await Venue.create(req.body);
+  return res.status(201).json(venue);
+}
+
+export async function deleteEvent(req, res) {
+  await Event.deleteOne({ eventId: req.params.eventId });
+  return res.status(204).send();
+}

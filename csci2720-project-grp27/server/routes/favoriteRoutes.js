2bf2cdd163434edef48717fa73fa5727403730dc
HTsz110 <132541006+HTsz110@users.noreply.github.com>
2025-12-06 02:25:58 +0800

    Task 4 done

--- csci2720-project-grp27/server/routes/favoriteRoutes.js
+++ csci2720-project-grp27/server/routes/favoriteRoutes.js
@@ -1,6 +1,11 @@
-import express from "express";
-const router = express.Router();
+const { Router } = require("express");
+const favoriteController = require("../controllers/favoriteController");
+const { authenticate, authorize } = require("../middleware/authMiddleware");
 
-// TODO: Implement in Task 6
+const router = Router();
 
-export default router;
+router.get("/", authenticate, authorize("user", "admin"), favoriteController.listFavorites);
+router.post("/", authenticate, authorize("user", "admin"), favoriteController.addFavorite);
+router.delete("/:favoriteId", authenticate, authorize("user", "admin"), favoriteController.removeFavorite);
+
+module.exports = router;

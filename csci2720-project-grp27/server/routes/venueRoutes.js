0d533b65d6371f62e831a57538364bd5f0fc2798
HTsz110 <132541006+HTsz110@users.noreply.github.com>
2025-12-06 19:34:45 +0800

    Task 6

--- csci2720-project-grp27/server/routes/venueRoutes.js
+++ csci2720-project-grp27/server/routes/venueRoutes.js
@@ -1,10 +1,10 @@
-const { Router } = require("express");
-const venueController = require("../controllers/venueController");
-const { authenticate, authorize } = require("../middleware/authMiddleware");
+import { Router } from "express";
+import * as venueController from "../controllers/venueController.js";
+import { authenticate, authorize } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", authenticate, authorize("user", "admin"), venueController.listVenues);
 router.get("/:venueId", authenticate, authorize("user", "admin"), venueController.getVenueById);
 
-module.exports = router;
+export default router;

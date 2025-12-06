3c667d6b701ca31a927b42955bab4c9818681572
HTsz110 <132541006+HTsz110@users.noreply.github.com>
2025-12-06 18:41:45 +0800

    Task 4

--- csci2720-project-grp27/server/models/User.js
+++ csci2720-project-grp27/server/models/User.js
@@ -1,18 +1,18 @@
-import { Schema, model, Document } from "mongoose";
+const { Schema, model } = require("mongoose");
 
-export interface IUser extends Document {
-  username: string;
-  passwordHash: string;
-  role: "user" | "admin";
-}
-
-const UserSchema = new Schema<IUser>(
+const UserSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     passwordHash: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
 
-export default model<IUser>("User", UserSchema);
+module.exports = model("User", UserSchema);

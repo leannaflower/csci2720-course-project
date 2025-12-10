fca4394d6025dadac4919f0391a9a33841fc8306
Daniel <157037600+Danbsnn@users.noreply.github.com>
2025-12-10 12:09:38 +0800

    Task 9 - Map.js

---
+++ csci2720-project-grp27/src/Map.js
@@ -0,0 +1,50 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+import "leaflet/dist/leaflet.css";
+
+export default function Map() {
+  const [venues, setVenues] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/api/venues")
+      .then((res) => res.json())
+      .then((data) => setVenues(data))
+      .catch((err) => console.error("Failed to fetch venues", err));
+  }, []);
+
+  // Default center of Hong Kong
+  const hkCenter = [22.3193, 114.1694];
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>All Venues on Map</h2>
+
+      <MapContainer
+        center={hkCenter}
+        zoom={12}
+        scrollWheelZoom={true}
+        style={{ height: "600px", width: "100%", borderRadius: "8px" }}
+      >
+        <TileLayer
+    attribution='&copy; OpenStreetMap & Carto'
+    url="https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png"
+    />
+
+        {venues
+          .filter((v) => v.latitude && v.longitude)
+          .map((venue) => (
+            <Marker
+              key={venue.id}
+              position={[venue.latitude, venue.longitude]}
+            >
+              <Popup>
+                <strong>{venue.name}</strong>
+                <br />
+                <a href={`/location/${venue.id}`}>View details</a>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
+    </div>
+  );
+}

7f6f082ec6f398cfe3b11c82da34dfa800e37a81
Daniel <157037600+Danbsnn@users.noreply.github.com>
2025-12-10 15:04:45 +0800

    task 9 - added markers
    note that cannot fetch from mongoDB due to no authorization

--- csci2720-project-grp27/src/Map.js
+++ csci2720-project-grp27/src/Map.js
@@ -2,11 +2,20 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
+const redPin = new L.Icon({
+  iconUrl: "https://maps.gstatic.com/mapfiles/ms2/micons/red.png",
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+});
+
+
 export default function Map() {
   const [venues, setVenues] = useState([]);
   
   useEffect(() => {
-    fetch("http://localhost:5000/api/venues")
+    fetch("http://localhost:5000/api/venues")  // Adjust PORT if necessary, should be same as server/.env
       .then((res) => res.json())
       .then((data) => setVenues(data))
       .catch((err) => console.error("Failed to fetch venues", err));
@@ -26,17 +35,18 @@ export default function Map() {
         style={{ height: "600px", width: "100%", borderRadius: "8px" }}
       >
         <TileLayer
-    attribution='&copy; OpenStreetMap & Carto'
-    url="https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png"
+    attribution='&copy; OpenStreetMap & Stadia Maps'
+    url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png"
     />
 
         {venues
           .filter((v) => v.latitude && v.longitude)
           .map((venue) => (
             <Marker
-              key={venue.id}
-              position={[venue.latitude, venue.longitude]}
-            >
+                key={venue.id}
+                position={[venue.latitude, venue.longitude]}
+                icon={redPin}
+                >
               <Popup>
                 <strong>{venue.name}</strong>
                 <br />
